6f2b5ef7aaae9bb498c8abdccb37fdcef14097a3
nessarz93 <126210620+nessarz93@users.noreply.github.com>
2023-05-23 03:00:22 +0000

    Inicio de proyecto

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,16 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<nav class="navbar bg-body-tertiary">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">
+					<i class="fa-regular fa-starship-freighter">Star Wars</i>
+				</a>
+				<button class="btn btn-primary d-flex justify-content-end" type="button">Favorites 0</button>
 			</div>
 		</nav>
+
 	);
 };
+
+export default Navbar;
