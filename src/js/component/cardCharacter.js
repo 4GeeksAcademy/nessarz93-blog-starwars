bd607f7d9792d94744d59002eafd59eff8a9f0a9
nessarz93 <126210620+nessarz93@users.noreply.github.com>
2023-05-25 02:56:19 +0000

    importar API

--- src/js/component/cardCharacter.js
+++ src/js/component/cardCharacter.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-export const CardCharacter = () => {
+export const CardCharacter = (props) => {
 	return (
         <>
-            <div className="card" style={{width: "18rem;"}}>
+            <div className="card" style={{width: "18rem"}}>
                 <img src="..." className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">{props.name}</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <Link to="#" className="btn btn-primary">Go somewhere</Link>
             </div>
             </div>
         </>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../styles/home.css";
 import CardCharacter from "../component/cardCharacter";
 
-export const Home = () => (
-	<>
+export const Home = () => {
+
+	const [people, setPeople] = useState([]);
+//Traer el API
+	function getPeople(){
+		console.log('getPeople')
+		fetch('https://swapi.dev/api/people')
+		.then( (response) => response.json())
+		.then( (data) => setPeople(data.results))
+	}
+
+//Iniciar API al cargar la página
+	useEffect(()=> {
+		getPeople()
+	},[]);
+
+	return (
+	<div className="text-center">
 		<h1>Characters</h1>
-		<div>
-			<CardCharacter/>
+		<div class="card-group">
+			{people.map( (ppl)=> <CardCharacter className="d-flex p-2" name={ppl.name}/>)}
 		</div>
-	</>
-);
+		
+	</div>
+	)
+};
